acd7d39d3c99c537e294a82744d65a701f259919
Anastasia <nastyapugach24@gmail.com>
2026-02-06 02:05:48 +0100

    Revert "add task solution"
    This reverts commit c7ae391434086ee884df79e25bc1a3238622404d.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,3 @@
 'use strict';
 
-const populationSpans = Array.from(document.querySelectorAll('.population'));
-const populationNumbers = populationSpans.map((el) => {
-  return Number(el.textContent.replaceAll(',', ''));
-});
-
-const total = populationNumbers.reduce((sum, current) => sum + current, 0);
-const average = total / populationNumbers.length;
-const totalFormatted = total.toLocaleString('en-US');
-const averageFormatted = average.toLocaleString('en-US');
-const totalContainer = document.querySelector('.total-population');
-const averageContainer = document.querySelector('.average-population');
-
-totalContainer.textContent = totalFormatted;
-averageContainer.textContent = averageFormatted;
+// write your code here
